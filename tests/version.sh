9109a79d8d6559e162c2f7c1c38c593af2f14069
Sheogorath <sheogorath@shivering-isles.com>
2017-11-04 21:57:32 +0100

    Fix paging for tags

--- tests/version.sh
+++ tests/version.sh
@@ -14,7 +14,7 @@ command_exits() { command -v $1 >/dev/null 2>&1 || { echo >&2 "I require $1 but 
 
 # Docker latest version tag
 # See: https://www.shivering-isles.com/helpful-shell-snippets-for-docker-testing-and-bootstrapping/
-docker_image_latest_tag() { REPO=$1; [ $(echo $REPO | grep '/' | wc -l) -eq 0 ] && REPO=library/$1; wget -q -O- https://registry.hub.docker.com/v2/repositories/${REPO}/tags | jq .results[].name | sort -V | sed -e 's/"//g' | grep -P '^[[:digit:]]+((.[[:digit:]]+)?.[[:digit:]]+)' | tail -1; }
+docker_image_latest_tag() { REPO=$1; [ $(echo $REPO | grep '/' | wc -l) -eq 0 ] && REPO=library/$1; wget -q -O- https://registry.hub.docker.com/v2/repositories/${REPO}/tags\?page\=1\&page_size\=250 | jq .results[].name | sed -e 's/"//g' | sort -V | grep -v "rc" | grep -P '^[[:digit:]]+((.[[:digit:]]+)?.[[:digit:]]+)$' | tail -1; }
 
 # Version comparison greater or equal
 # See: https://www.shivering-isles.com/helpful-shell-snippets-for-docker-testing-and-bootstrapping/
@@ -31,5 +31,7 @@ docker_base_name() { cat "$1" | grep FROM | sed -e "s/FROM[^[:alpha:]]//g" -e "s
 command_exits wget
 
 [ -e "$DOCKERFILE" ] || { echo >&2 "File '$DOCKERFILE' doesn't exist.  Aborting."; exit 1; }
+
+echo $(docker_image_latest_tag `docker_base_name "$DOCKERFILE"` | cut -d. -f1-2)
 
 version_ge $(docker_base_version "$DOCKERFILE") $(docker_image_latest_tag `docker_base_name "$DOCKERFILE"` | cut -d. -f1-2) && echo "Base image is up to date! Test successful." || { echo >&2 "A newer base image is available! Please update."; exit 1; }
